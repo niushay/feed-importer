a84fdc14fa546462d942106a64683b795f76c14e
Niusha Yousefi <niusha.mech@gmail.com>
2025-07-01 20:32:03 +0200

    Create Product model and migration for products table

---
+++ database/migrations/2025_07_01_153521_create_productions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productions', function (Blueprint $table) {
+            $table->id();
+            $table->string('gtin')->unique();
+            $table->string('language', 2)->nullable();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('picture')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->integer('stock')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productions');
+    }
+};
